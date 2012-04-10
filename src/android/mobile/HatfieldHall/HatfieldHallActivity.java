779bae73132c9315547f9889f990f8eb02a12065
Andy Milluzzi <edrocket734@gmail.com>
2012-04-10 11:39:19 -0400

    initial commit with project setup

---
+++ src/android/mobile/HatfieldHall/HatfieldHallActivity.java
@@ -0,0 +1,13 @@
+package android.mobile.HatfieldHall;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HatfieldHallActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
