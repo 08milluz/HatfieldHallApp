640b029d2f928a2d9198010fabd65e0a7e83f1f2
Ali Almajed <almajeas@msn.com>
2012-04-14 23:42:27 -0400

    added main classes, tabs, and tab icons

--- src/android/mobile/HatfieldHall/HatfieldHallActivity.java
+++ src/android/mobile/HatfieldHall/HatfieldHallActivity.java
@@ -1,13 +1,48 @@
 package android.mobile.HatfieldHall;
 
-import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class HatfieldHallActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-}
+public class HatfieldHallActivity extends TabActivity{
+
+		/** Called when the activity is first created. */
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.main);
+
+			TabHost tabHost = getTabHost();
+
+			// Tab for Home
+			TabSpec homeSpec = tabHost.newTabSpec("Home");
+			// setting Title and Icon for the Tab
+			homeSpec.setIndicator("Home",
+					getResources().getDrawable(R.drawable.icon_home_tab));
+			Intent homeIntent = new Intent(this, HomeActivity.class);
+			homeSpec.setContent(homeIntent);
+
+			// Tab for Find Seat
+			TabSpec seatSpec = tabHost.newTabSpec("Find Seat");
+			seatSpec.setIndicator("Find Seat",
+					getResources().getDrawable(R.drawable.icon_search_tab));
+			Intent seatIntent = new Intent(this, SeatActivity.class);
+			seatSpec.setContent(seatIntent);
+
+			// Tab for Venue
+			TabSpec venueSpec = tabHost.newTabSpec("Venue");
+			venueSpec.setIndicator("Venue",
+					getResources().getDrawable(R.drawable.icon_venue_tab));
+			Intent venueIntent = new Intent(this, VenueActivity.class);
+			venueSpec.setContent(venueIntent);
+
+			// Adding all TabSpec to TabHost
+			tabHost.addTab(homeSpec); // Adding Home tab
+			tabHost.addTab(seatSpec);//Adding Find-Seat tab
+			tabHost.addTab(venueSpec); // Adding Venue tab
+
+
+		}
+	}
