22e64b52327aea4c5f8013860113d4a307721595
Ali Almajed <almajeas@msn.com>
2012-04-19 15:54:10 -0400

    add L10N successfully

--- src/android/mobile/HatfieldHall/HatfieldHallActivity.java
+++ src/android/mobile/HatfieldHall/HatfieldHallActivity.java
@@ -15,28 +15,33 @@ public class HatfieldHallActivity extends TabActivity{
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.main);			
 			TabHost tabHost = getTabHost();
-
+			
+//			// Tab for Home
+//			TabSpec homeSpec = tabHost.newTabSpec(R.string.Home);
+//			// setting Title and Icon for the Tab
+//			homeSpec.setIndicator(R.string.Home,
+//					getResources().getDrawable(R.drawable.icon_home_tab));
+//			Intent homeIntent = new Intent(this, HomeActivity.class);
+//			homeSpec.setContent(homeIntent);
+			
 			// Tab for Home
-			String home = ((EditText) findViewById(R.string.Home)).toString();
-			TabSpec homeSpec = tabHost.newTabSpec(home);
+			TabSpec homeSpec = tabHost.newTabSpec(getString(R.string.home));
 			// setting Title and Icon for the Tab
-			homeSpec.setIndicator(home,
+			homeSpec.setIndicator(getString(R.string.home),
 					getResources().getDrawable(R.drawable.icon_home_tab));
 			Intent homeIntent = new Intent(this, HomeActivity.class);
 			homeSpec.setContent(homeIntent);
 
 			// Tab for Find Seat
-			String findSeat = ((EditText) findViewById(R.string.Seat)).toString();
-			TabSpec seatSpec = tabHost.newTabSpec(findSeat);
-			seatSpec.setIndicator(findSeat,
+			TabSpec seatSpec = tabHost.newTabSpec(getString(R.string.find_seat));
+			seatSpec.setIndicator(getString(R.string.find_seat),
 					getResources().getDrawable(R.drawable.icon_search_tab));
 			Intent seatIntent = new Intent(this, SeatActivity.class);
 			seatSpec.setContent(seatIntent);
 
 			// Tab for Venue
-			String venue = ((EditText) findViewById(R.string.Venue)).toString();
-			TabSpec venueSpec = tabHost.newTabSpec(venue);
-			venueSpec.setIndicator(venue,
+			TabSpec venueSpec = tabHost.newTabSpec(getString(R.string.venue));
+			venueSpec.setIndicator(getString(R.string.venue),
 					getResources().getDrawable(R.drawable.icon_venue_tab));
 			Intent venueIntent = new Intent(this, VenueActivity.class);
 			venueSpec.setContent(venueIntent);
