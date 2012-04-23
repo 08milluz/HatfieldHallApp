d4fab12c5a31464fb9a00121a64479e76fa440d6
Caleb Drake <drakecb@rose-hulman.edu>
2012-04-23 19:20:29 -0400

    Current SeatActivity Layout

--- HatfieldHallApp/src/android/mobile/HatfieldHall/SeatActivity.java
+++ HatfieldHallApp/src/android/mobile/HatfieldHall/SeatActivity.java
@@ -2,6 +2,8 @@ package android.mobile.HatfieldHall;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SeatActivity extends Activity {
 	
@@ -9,6 +11,10 @@ public class SeatActivity extends Activity {
 	 public void onCreate(Bundle savedInstanceState) {
 	       super.onCreate(savedInstanceState);
 	       setContentView(R.layout.seat);
-	   }
-
+	   
+	       ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.rows, android.R.layout.simple_spinner_item );
+	       adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+	       Spinner s = (Spinner) findViewById( R.id.rowSpinner );
+	       s.setAdapter( adapter );
+	 }
 }
