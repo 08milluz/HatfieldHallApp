f7e42130e3888a694c71e839ca19314d81cf2997
Andy Milluzzi <edrocket734@gmail.com>
2012-04-23 22:33:25 -0400

    initial parser test code

---
+++ HatfieldHallApp/src/android/mobile/HatfieldHall/WebsiteParser.java
@@ -0,0 +1,14 @@
+package android.mobile.HatfieldHall;
+
+
+/* This class will parse the
+ * Hatfield Hall website
+ * http://hatfieldhall.com and save
+ * a xml file of the current show
+ * information.
+ */
+
+
+public class WebsiteParser {
+
+}
---
+++ HatfieldHallTest/src/android/mobile/HatfieldHall/test/WebsiteParserTest.java
@@ -0,0 +1,16 @@
+package android.mobile.HatfieldHall.test;
+
+/* This class will test the WebsiteParser
+ * class from the Hatfield Hall app.
+ */
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class WebsiteParserTest {
+
+}
