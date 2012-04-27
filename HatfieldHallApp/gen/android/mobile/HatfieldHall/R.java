8a2b281dea42f3e0bbb14a58fce310fc229e8809
Caleb Drake <drakecb@rose-hulman.edu>
2012-04-27 15:48:47 -0400

    SeatActivity Assertion Tests updated; current SeatActivity is pannable, and the Hashtable which takes a key consisting of the seatRow & seatNumber and returns the coordinate of the seat. Proposing extension of due date to: Friday, 2012 May 4; due to difficulties overlaying the floorMap with a visual reference

--- HatfieldHallApp/gen/android/mobile/HatfieldHall/R.java
+++ HatfieldHallApp/gen/android/mobile/HatfieldHall/R.java
@@ -8,19 +8,42 @@
 package android.mobile.HatfieldHall;
 
 public final class R {
+    public static final class array {
+        public static final int rows=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
-        public static final int home=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int icon_home_tab=0x7f020002;
-        public static final int icon_search_tab=0x7f020003;
-        public static final int icon_venue_tab=0x7f020004;
-        public static final int search=0x7f020005;
-        public static final int venue=0x7f020006;
+        public static final int floor_map=0x7f020000;
+        public static final int hatfieldlogo=0x7f020001;
+        public static final int home=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int icon_home_tab=0x7f020004;
+        public static final int icon_search_tab=0x7f020005;
+        public static final int icon_venue_tab=0x7f020006;
+        public static final int search=0x7f020007;
+        public static final int venue=0x7f020008;
     }
     public static final class id {
-        public static final int textView1=0x7f050000;
+        public static final int floorMap=0x7f060006;
+        public static final int linearLayout1=0x7f060008;
+        public static final int rowSpinner=0x7f060002;
+        public static final int seatSearch=0x7f060004;
+        public static final int seat_num=0x7f060003;
+        public static final int tableRow1=0x7f060001;
+        public static final int tableRow2=0x7f060005;
+        public static final int textView1=0x7f060000;
+        public static final int venue_address=0x7f06000b;
+        public static final int venue_address_layout=0x7f06000a;
+        public static final int venue_graphic=0x7f060009;
+        public static final int venue_hall_address=0x7f06000c;
+        public static final int venue_hall_hours=0x7f060012;
+        public static final int venue_hall_phone=0x7f06000f;
+        public static final int venue_hours=0x7f060011;
+        public static final int venue_hours_layout=0x7f060010;
+        public static final int venue_phone=0x7f06000e;
+        public static final int venue_phone_layout=0x7f06000d;
+        public static final int zoomControls1=0x7f060007;
     }
     public static final class layout {
         public static final int home=0x7f030000;
@@ -29,9 +52,18 @@ public final class R {
         public static final int venue=0x7f030003;
     }
     public static final class string {
+        public static final int address=0x7f040005;
         public static final int app_name=0x7f040000;
         public static final int find_seat=0x7f040001;
+        public static final int hall_address=0x7f040006;
+        public static final int hall_hours=0x7f04000a;
+        public static final int hall_phone=0x7f040008;
         public static final int home=0x7f040002;
+        public static final int hours=0x7f040009;
+        public static final int info=0x7f040004;
+        public static final int phone=0x7f040007;
+        public static final int search=0x7f04000c;
+        public static final int seat_number=0x7f04000b;
         public static final int venue=0x7f040003;
     }
 }
