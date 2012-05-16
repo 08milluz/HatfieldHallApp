d9ab783c64aeedd8a47fa7f647f1ea78910c5bc2
Ali Almajed <almajeas@msn.com>
2012-05-16 02:50:08 -0400

    fixed picture size

--- HatfieldHallApp/src/android/mobile/HatfieldHall/ShowsListAdapter.java
+++ HatfieldHallApp/src/android/mobile/HatfieldHall/ShowsListAdapter.java
@@ -1,69 +1,77 @@
 package android.mobile.HatfieldHall;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-
-public class ShowsListAdapter extends BaseAdapter{
+public class ShowsListAdapter extends BaseAdapter {
 	private ArrayList<Event> list;
 	private Context context;
 	private LayoutInflater inflater;
 
-
-	public ShowsListAdapter (ArrayList<Event> showsList, Context c){
+	public ShowsListAdapter(ArrayList<Event> showsList, Context c) {
 		this.list = showsList;
 		this.context = c;
 	}
-	
-	public int getCount(){
+
+	public int getCount() {
 		return this.list.size();
 	}
-	
-	public Object getItem (int position){
+
+	public Object getItem(int position) {
 		return list.get(position);
 	}
-	
-	public View getView (int position, View convertView, ViewGroup parent){
+
+	public View getView(int position, View convertView, ViewGroup parent) {
 		inflater = (LayoutInflater) this.context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		LinearLayout line = (LinearLayout) inflater.inflate(
 				R.layout.show_entry, null);
-		
-		int [] titleDepth = {0 , 1 , 0};
-		int [] infoDepth = {0 , 1 , 1};
-		int [] picDepth = {0 , 0};
-		int [] buttonDepth = {0 , 1 , 2};
-		
-		//ImageView image = (ImageView) expand(line , picDepth);
+
+		int[] titleDepth = { 0, 1, 0 };
+		int[] infoDepth = { 0, 1, 1 };
+		int[] picDepth = { 0, 0 };
+		int[] buttonDepth = { 0, 1, 2 };
+
 		TextView title = (TextView) expand(line, titleDepth);
 		TextView info = (TextView) expand(line, infoDepth);
-		
-		Button buyTickets = (Button) expand (line, buttonDepth);
-		
+		ImageView image = (ImageView) expand(line, picDepth);
+		Button buyTickets = (Button) expand(line, buttonDepth);
+
 		final String name = this.list.get(position).name;
 		title.setText(this.list.get(position).name);
-		
 		info.setText(this.list.get(position).dates);
 		
+		Drawable drawable = LoadImageFromWeb("http://hatfieldhall.com/images/featured/Phantom_480x320.png");
+		Bitmap d = ((BitmapDrawable)drawable).getBitmap();
+	    Bitmap bitmapOrig = Bitmap.createScaledBitmap(d, 128, 128, false);
+		Log.d("ali", this.list.get(position).link);
+		image.setImageBitmap(bitmapOrig);
+
 		buyTickets.setOnClickListener(new View.OnClickListener() {
 
-			
 			public void onClick(View arg0) {
 				Log.d("testMail", name);
 				HomeActivity home = (HomeActivity) context;
 				home.sendEmail(name);
-				
-			}});
-		
+
+			}
+		});
+
 		return line;
 	}
 
@@ -71,14 +79,22 @@ public class ShowsListAdapter extends BaseAdapter{
 		for (int i = 0; i < depth.length - 1; i++) {
 			root = (LinearLayout) root.getChildAt(depth[i]);
 		}
-		return  root.getChildAt(depth[depth.length - 1]);
+		return root.getChildAt(depth[depth.length - 1]);
 	}
 
-	
 	public long getItemId(int i) {
 		return i;
 	}
-	
-	
-	     
+
+	private Drawable LoadImageFromWeb(String url) {
+		try {
+			InputStream is = (InputStream) new URL(url).getContent();
+			Drawable d = Drawable.createFromStream(is, "src name");
+			return d;
+		} catch (Exception e) {
+			System.out.println("Exc=" + e);
+			return null;
+		}
+	}
+
 }
